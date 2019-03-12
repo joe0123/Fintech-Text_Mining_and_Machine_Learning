c167020d24641ce1d267ab423a45b9998df5e62f
joe0123 <34242614+joe0123@users.noreply.github.com>
2019-03-13 01:51:18 +0800

    download .csv (1 success)

--- HW1/crawler.py
+++ HW1/crawler.py
@@ -1,17 +1,57 @@
 import sys
+import os
 import pandas as pd
 import requests as req
 from bs4 import BeautifulSoup as bs
+import urllib.request as url
+
+def proshares_dl(homepage, filename):
+    #loading point
+    ldp = soup.find("a", string = "NAV History").get('href')
+    try:
+        url.urlretrieve(ldp, filename)
+    except:
+        print("Downloading", ldp, "fails")
+        return
+
+#wait# convert xls to csv (module "xlrd" should be installed beforehand)
+    #data_xls = pd.read_excel(filename)
+    #data_xls.to_csv(filename - "xls" + "csv", encoding='utf-8', index=False)
+    #os.remove(filename)
 
 df = pd.read_csv(sys.argv[1])
 ETFs = df["Symbol"].values  # list of all etfs' symbol names
-whole_name = df["ETF Name"].values  # list of all etfs' whole names
 
 for etf in ETFs:
-    r = req.get("https://www.etf.com/" + etf)   # download contents in the website
-    if r.status_code == req.codes.ok:   # to check if req.get is successful
-        soup = bs(r.text, 'html.parser')    # handle html code with BeautifulSoup
-        href = soup.find("a", string="Fund Home Page").get('href')  # find hypertext reference
-        #DEBUG# print(etf, href)
+#Find url of Homepage
+    try:
+        r = req.get("https://www.etf.com/" + etf, headers={'User-Agent': 'Mozilla/5.0'})
+    except:
+        print("\"https://www.etf.com/" + etf + "\"", "is not found")
+        continue
+    if r.status_code != req.codes.ok:
+        print("a")
+        continue
+    #handle html code with BeautifulSoup
+    soup = bs(r.text, 'html.parser')
+    homepage = soup.find("a", string="Fund Home Page").get('href')
+    #DEBUG# print(etf, homepage)
+    
+#Download data
+    try:
+        r = req.get(homepage)
+    except:
+        print("\""+homepage+"\"", "is not found")
+        continue
+    if r.status_code != req.codes.ok:
+        continue
+    soup = bs(r.text, 'html.parser')
+    #the url of ETF's family (like iShares, SPDR...)
+    family = homepage.split('/')[2]
+    if family == "www.proshares.com":
+        proshares_dl(homepage, etf + ".csv")
+    else:
+        continue
+
         
 
