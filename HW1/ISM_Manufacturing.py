e2d30bbc09df8d80f16dbe55ea64289848fe8f7e
andy94077 <andy94077@gmail.com>
2019-03-18 00:43:45 +0800

    [hw1]finish ISM

---
+++ HW1/ISM_Manufacturing.py
@@ -0,0 +1,15 @@
+import os
+import urllib.request as url
+import pandas as pd
+
+download_dir='download'
+if not os.path.exists(download_dir):
+	os.mkdir(download_dir)
+url.urlretrieve('https://www.quandl.com/api/v3/datasets/FRED/NAPMPRI.csv?api_key=Gy65phJwPp3dpbzSY7sR&collapse=monthly&end_date=2018-05-01',
+				'./'+download_dir+'/ISM_Manufacturing.csv')
+
+df = pd.read_csv(download_dir+'/ISM_Manufacturing.csv')
+df.columns = ['Date', 'Value']
+df = df.reindex(index=df.index[::-1])
+df.index=df.index[::-1]
+print(df.head(20))
