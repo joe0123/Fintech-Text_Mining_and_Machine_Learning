19910f62bbfee386b6bf192084951f382d3940ba
andy94077 <andy94077@gmail.com>
2019-03-18 00:49:06 +0800

    [hw1]now ISM.py will download the latest csv

--- HW1/ISM_Manufacturing.py
+++ HW1/ISM_Manufacturing.py
@@ -1,11 +1,12 @@
 import os
+import datetime
 import urllib.request as url
 import pandas as pd
 
 download_dir='download'
 if not os.path.exists(download_dir):
 	os.mkdir(download_dir)
-url.urlretrieve('https://www.quandl.com/api/v3/datasets/FRED/NAPMPRI.csv?api_key=Gy65phJwPp3dpbzSY7sR&collapse=monthly&end_date=2018-05-01',
+url.urlretrieve('https://www.quandl.com/api/v3/datasets/FRED/NAPMPRI.csv?api_key=Gy65phJwPp3dpbzSY7sR&collapse=monthly&end_date='+str(datetime.datetime.now().date()),
 				'./'+download_dir+'/ISM_Manufacturing.csv')
 
 df = pd.read_csv(download_dir+'/ISM_Manufacturing.csv')
