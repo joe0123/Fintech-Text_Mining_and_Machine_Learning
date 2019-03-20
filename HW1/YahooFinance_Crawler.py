73908d5ec8b47356b7bceabd4c1266f0bbf73658
andy94077 <andy94077@gmail.com>
2019-03-21 00:11:36 +0800

    [hw1]revise yahoo.py

--- HW1/YahooFinance_Crawler.py
+++ HW1/YahooFinance_Crawler.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os,glob,sys,time
 from datetime import datetime
 import pandas as pd
 from selenium import webdriver
@@ -24,26 +23,32 @@ class YahooFinanceCrawler(object):
 
 	def get_etf_csv(self, etf_name):
 		self.driver.get('https://finance.yahoo.com/quote/'+etf_name+'/history?period1=1451577600&period2='+self.timestamp+'&interval=1d&filter=history&frequency=1d')
+		#print('downloading',etf_name)
 		try:
 			button=WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.XPATH,"//span[contains(text(),'Download Data')]")))
-			self.driver.implicitly_wait(2)
+			#self.driver.implicitly_wait(1)
 			button.click()
 		except:
 			print(etf_name,'is not reachable.',file=sys.stderr)
 
 def main():
 	df = pd.read_csv(sys.argv[1])
-	etf_list = list(df["Symbol"].values)
-
+	etf_list = list(df["Symbol"][df['Inception']<='2015/12/31'].values)
+	#print(etf_list)
 	crawler=YahooFinanceCrawler()
 	for etf in etf_list:
 		crawler.get_etf_csv(etf)
+		time.sleep(1)
 
+	df_list = [pd.read_csv(next(glob.iglob('tmp/*.csv')))['Date']]
 	for etf in etf_list:
 		if not os.path.exists('tmp/' + etf + '.csv'):
 			crawler.get_etf_csv(etf)
+		if os.path.exists('tmp/' + etf + '.csv'):  #if it still does not exist, ignore it
+			df_list.append(pd.read_csv('tmp/' + etf + '.csv')['Adj Close'])
+		else:
+			etf_list.remove(etf)
 	
-	df_list = [pd.read_csv('tmp/'+etf_list[0]+'.csv')['Date']]+[pd.read_csv('tmp/' + etf + '.csv')['Adj Close'] for etf in etf_list]
 	df = pd.concat(df_list, axis=1, ignore_index=True)
 	df.columns = ['Date'] + etf_list
 	print(df.head())
