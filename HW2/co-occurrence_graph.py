11ecfb9827a9a47786277d0efdfb7d4137977124
joe0123 <34242614+joe0123@users.noreply.github.com>
2019-04-09 12:31:57 +0800

    Update co-occurrence_graph.py

--- HW2/co-occurrence_graph.py
+++ HW2/co-occurrence_graph.py
@@ -14,13 +14,11 @@ nm_list = df["label_headers"].values
 co_matrix = df.drop(columns=["label_headers"]).values
 freq_list = [209, 116, 172, 292, 249, 92, 67, 193, 11, 3, 0, 24, 37, 24, 0, 0, 16, 4, 1, 1, 165, 30, 16, 64, 500, 359, 144, 281, 98, 71, 333, 41, 176, 350, 793, 426, 406, 911, 107, 266, 2, 190, 79, 1099, 146, 6, 546, 75, 805, 97, 14, 10, 111, 112, 17, 169, 527, 912, 141, 37, 652, 817, 293, 15, 9, 1373, 311, 101, 29, 20, 17, 31, 77, 305, 678, 695, 71, 17, 53, 442, 1203, 0, 23, 53, 413, 1297, 12, 128, 424, 243, 979, 2, 1, 653, 1080, 50, 5, 105, 445, 580, 651, 47, 193, 351, 437, 758]
 
-
 G = nx.Graph()
 for i in range(len(co_matrix)):
     if freq_list[i] != 0:
         G.add_node(nm_list[i], size = freq_list[i])
 
-edge_list = []
 for i in range(len(co_matrix)):
     for j in range(len(co_matrix)):
         if (i != j) and (co_matrix[i][j] != 0.0):
