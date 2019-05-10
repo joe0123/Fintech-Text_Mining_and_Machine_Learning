f080a53c3cccf20608538c52e35fda456c4e9aa2
joe0123 <34242614+joe0123@users.noreply.github.com>
2019-05-10 11:49:39 +0800

    Update histogram_percentage.py

--- HW2/histogram_percentage.py
+++ HW2/histogram_percentage.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 original_data = pd.read_csv(sys.argv[1]).drop(columns=["label_headers"]).values.flatten()
+#condition
 data = [i for i in original_data if i <= 0]
-print(len(data))
+#print(len(data))
+
 _, axs=plt.subplots()
-#cumulative = -1 if all i in data > 0; otherwise, cumulative = 1
+#condition: cumulative = -1 if all i in data > 0; otherwise, cumulative = 1
 axs.hist(data, bins = 20, cumulative = 1, edgecolor='black', density=True)
 axs.yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter(xmax=1))
 plt.show()
