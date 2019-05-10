7bbd1517070fceda6e4dfb49bebeb94880256130
joe0123 <34242614+joe0123@users.noreply.github.com>
2019-05-10 12:23:01 +0800

    add graphs

--- HW2/co-occurrence_graph.py
+++ HW2/co-occurrence_graph.py
@@ -21,13 +21,14 @@ def drawing_graph(G, nodesize, edgesize):
 df_comatrix = pd.read_csv(sys.argv[1], index_col="label_headers", encoding="utf-8")
 df_freq = pd.read_csv(sys.argv[2], index_col="label_headers", encoding="utf-8")
 #draw only these specific nodes into graph
-spec_nodes = [i for i in df_comatrix.index.values if i.find("年收入") != -1]
+spec_nodes = [i for i in df_comatrix.index.values]
+#spec_nodes = [i for i in df_comatrix.index.values if i.find("年收入") != -1]
 
 G = nx.Graph()
 for i in spec_nodes:
     for j in df_comatrix.columns.values:
 #condition
-        if i != j and df_comatrix.ix[i, j] >= 0.8:
+        if i != j and df_comatrix.ix[i, j] >= 0.6:
             G.add_edge(i, j)
 
 if len(sys.argv) == 4:
@@ -36,10 +37,10 @@ if len(sys.argv) == 4:
     for i in spec_nodes:
         for j in G.nodes:
 #condition according to diff_matrix
-            if i != j and (df_diffmatrix.ix[i, j] >= 0.6 or df_diffmatrix.ix[j, i] >= 0.6):
+            if i != j and (df_diffmatrix.ix[i, j] <= -0.6 or df_diffmatrix.ix[j, i] <= -0.6):
                 sub_edge.append((i, j))
-                print(i, j, df_comatrix.ix[i, j])
+                #print(i, j, df_comatrix.ix[i, j])
     H = G.edge_subgraph(sub_edge)
-    drawing_graph(H, [df_freq.ix[u, "freq"] for u in H.nodes], [df_comatrix.ix[u, v]*5 for u, v in H.edges])
+    drawing_graph(H, [df_freq.ix[u, "freq"] for u in H.nodes], [df_comatrix.ix[u, v]*2 for u, v in H.edges])
 else:
-    drawing_graph(G, [df_freq.ix[u, "freq"] for u in G.nodes], [df_comatrix.ix[u, v]*5 for u, v in G.edges])
+    drawing_graph(G, [df_freq.ix[u, "freq"] for u in G.nodes], [df_comatrix.ix[u, v]*2 for u, v in G.edges])
