641c5d7b009e553f3e23478e221fbfa8052aea43
andy94077 <andy94077@gmail.com>
2019-05-14 00:27:11 +0800

    [hw4] Finish three evaluation

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,35 +1,52 @@
-import os,glob,sys,time
+import sys
 from datetime import datetime
-from YahooFinance_Crawler import YahooFinanceCrawler
 import pandas as pd
 import numpy as np
 from scipy import stats
+from scipy.optimize import fsolve
+
+from YahooFinance_Crawler import YahooFinanceCrawler
+
 def get_return(table):
 	return (table[1:,:] - table[:-1,:]) / table[:-1,:]
 
-def ASKSR(table, r):
-	#print(table)
+def ASSR(table, r, delta_t):
 	mu = np.mean(table, axis=0)
-	sigma = np.sqrt(np.var(table, axis=0))
-	K = stats.kurtosis(table, axis=0)
-	S = stats.skew(table, axis=0)
-	print(mu,sigma,K,S,sep='\n')
+	sigma = np.sqrt(np.var(table, axis=0,ddof=1))
+	SR = (mu-r)/sigma*np.sqrt(delta_t)
+	skew = stats.skew(table, axis=0)
+
+	return SR * np.sqrt(1 + 2 / 3 * skew * SR)
+
+def Sharpe_Omega(table, r):
+	omega=[]
+	for t in table.T:
+		hist, bins = np.histogram(t, bins=np.arange(np.min(t), np.max(t) + 1e-3,1e-3),density=True)
+		interval=np.diff(bins)
+		pdf=hist*interval
+		cdf=np.cumsum(pdf)
+		index=np.searchsorted(bins,r)-1
+		omega.append((np.max(t) - r - np.sum(interval[index:] * cdf[index:])) / np.sum(interval[:index] * cdf[:index]))
+		
+	return np.array(omega)-1
+
+def riskiness(table, r):
+	def f1(x, *args):
+		return np.sum(np.exp(-args[0] * x)) - args[0].size
 	
-	k4s9 = 3 * K - 4 * S ** 2 - 9
-	k5s9 = 3 * K - 5 * S ** 2 - 9
-	print(k4s9,k5s9,sep='\n')
-	
-	alpha = 3 * np.sqrt(k4s9) / sigma ** 2 / k5s9
-	beta = 3 * S / sigma / k5s9
-	eta = mu - 3 * S * sigma / k4s9
-	delta = 3 * sigma * np.sqrt(k5s9) / k4s9
-	a_star = beta + alpha * (eta - r) / np.sqrt(delta ** 2 + (eta - r)** 2)
+	Q = []
+	table -= r
+	for t in table.T:
+		guess = 10
+		while guess < 1e6:
+			ag = fsolve(f1, guess, args=(t))[0]
+			if ag != guess:
+				break
+			guess *= 10
+		Q.append(np.exp(-ag))
+	return np.array(Q)
 
-	#print('\n',alpha,beta,eta,delta,a_star,sep='\n')
-
-	return np.sqrt(2 * a_star * (eta - r) - delta * (np.sqrt(alpha ** 2 - beta ** 2) - np.sqrt(alpha ** 2 - (beta - a_star)** 2)))
-
-def ETF_evaluation(etf_list, frequency, method, download_dir='tmp', has_download=False):
+def ETF_evaluation(etf_list, frequency, method, download_dir='tmp', has_download=False, **kwargs):
 	'''@parameters:
 		frequency: 'month', 'week', 'day'
 		method:    function(array, rate)
@@ -37,7 +54,11 @@ def ETF_evaluation(etf_list, frequency, method, download_dir='tmp', has_download
 	crawler = YahooFinanceCrawler(download_dir=download_dir)
 	df, _ = crawler.get_etf_df(etf_list, 'Adj Close', frequency, has_download)
 	#print(df)
-	result = method(get_return(df.values), 0.0005)
+	if kwargs:
+		result = method(get_return(df.values), 0.0243, **kwargs)
+	else:
+		result = method(get_return(df.values), 0.0243)
+
 	order = result.argsort()[::-1]
 	df = pd.DataFrame(etf_list[order].reshape(-1, 1), columns=['ETF_Symbol'], index=np.arange(1, result.shape[0] + 1))
 	df.index.name = 'Ranking'
@@ -50,9 +71,25 @@ date=datetime.today().date()
 etf_list = df["Symbol"][df['Inception'] <= '%d/%d/%d' % (date.year-3, date.month, date.day)].values
 #print(etf_list)
 
-month_df=ETF_evaluation(etf_list,'month',ASKSR, has_download= True)
-print(month_df.head(20))
+ASSR_month_df=ETF_evaluation(etf_list,'month',ASSR, has_download= True,delta_t=1/12)
+print('ASSR month',ASSR_month_df.head(20),sep='\n')
 
 
-week_df = ETF_evaluation(etf_list,'week',ASKSR,download_dir='tmp_week',has_download=True)
-print(week_df.head(20))
+ASSR_week_df = ETF_evaluation(etf_list,'week',ASSR,download_dir='tmp_week',has_download=True,delta_t=1/52)
+print('ASSR week',ASSR_week_df.head(20),sep='\n')
+
+
+Omega_month_df = ETF_evaluation(etf_list, 'month', Sharpe_Omega, has_download=True)
+print('Omega month',Omega_month_df.head(20),sep='\n')
+
+
+Omega_week_df = ETF_evaluation(etf_list, 'week', Sharpe_Omega, has_download=True)
+print('Omega week',Omega_week_df.head(20),sep='\n')
+
+
+riskiness_month_df = ETF_evaluation(etf_list, 'month', riskiness, has_download=True)
+print('riskiness month',riskiness_month_df.head(20),sep='\n')
+
+
+riskiness_week_df = ETF_evaluation(etf_list, 'week', riskiness, has_download=True)
+print('riskiness week',riskiness_week_df.head(20),sep='\n')
