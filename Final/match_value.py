87b40d73856993084d1cda2471c0144162fe7572
joe0123 <34242614+joe0123@users.noreply.github.com>
2019-06-05 22:26:03 +0800

    create net_value.csv

--- Final/match_value.py
+++ Final/match_value.py
@@ -20,22 +20,28 @@ class match_value():
         for i in df.index:
             if i > df.index[0] and df.ix[i, "remote"] == df.ix[i-1, "remote"]:
                 df.ix[i, df.columns[2]:df.columns[-1]] = df.ix[i-1, df.columns[2]:df.columns[-1]]
+                #print(i, "is done!")
                 continue
 
             if df.ix[i, "remote"][:4] != "http":
-                self.driver.get("https://fund.cnyes.com/search/?fundName=%E9%87%8E%E6%9D%91&order=displayNameLocal&page="+str(page_dict[df.ix[i, "remote"]]))
-                self.driver.get(self.driver.find_element_by_link_text(df.ix[i, "remote"]).get_attribute("href"))
+                self.driver.get("https://fund.cnyes.com/search/?fundName=%E9%87%8E%E6%9D%91&order=displayNameLocal&page="+str(page_dict[df.ix[i, "remote"].replace("?", "類")]))
+                self.driver.get(self.driver.find_element_by_link_text(df.ix[i, "remote"].replace("?", "類")).get_attribute("href"))
             else:
                 self.driver.get(df.ix[i, "remote"])
             
             end = False
             for target in df.columns[2:]:
                 while not end:
-                    time.sleep(self.time)
-                    dates = [j.text for j in self.driver.find_elements_by_xpath("//*[@id=\"content\"]/div/div/div[2]/div[2]/main[2]/section/div[2]/section[3]/table/tbody/tr/td[@class=\"_1JRsZ\"]")]
+                    try:
+                        time.sleep(self.time)
+                        dates = [j.text for j in self.driver.find_elements_by_xpath("//*[@id=\"content\"]/div/div/div[2]/div[2]/main[2]/section/div[2]/section[3]/table/tbody/tr/td[@class=\"_1JRsZ\"]")]
+                    except Exception as e:
+                        print(e)
+                        return df
+
                     if int("".join(dates[0].split("/"))) < int("".join(target.split("/"))):
-                        print(target, dates[0])
                         break
+
                     try:
                         found = dates.index(target)
                     except: 
@@ -49,6 +55,7 @@ class match_value():
                         break
                 if end:
                     break
+            #print(i, "is done!")
         return df
 
 if __name__ == '__main__':
@@ -74,7 +81,6 @@ if __name__ == '__main__':
     df.index = range(len(df))
     for i in ['2018/12/14', '2018/11/15', '2018/10/16', '2018/09/14', '2018/08/15', '2018/07/17', '2018/06/15', '2018/05/15', '2018/04/17', '2018/03/15', '2018/02/09', '2018/01/16']:
         df[i] = [None]*len(df.index)
-    df = match_value(2).get_value(page_dict, df.head(1))
+    df = match_value(1.5).get_value(page_dict, df)
     #print(df)
-    df.drop("remote", axis=1)
-    df.to_csv("net_value.csv", encoding="big5", index=False)
+    df.drop(columns="remote").to_csv("net_value3.csv", encoding="big5", index=False)
